fb4b89ff1f7fec6e5e119278498d66dc1bdbcdb0
Amine Benabdallah <amine.benabdallah017@gmail.com>
2020-04-11 18:07:43 +0100

    updating config.js
    updating config file
    updating config file
    :wq

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    'secretKey':'1234-5678-9123-4567',
+    'secretKey':'***************',
     'mongoUrl':'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '297326611249874',
-        clientSecret:'c6128519aae77b6ce9959fb53291c0cf'
+        clientId: '***********',
+        clientSecret:'***************'
     }
 }
 
